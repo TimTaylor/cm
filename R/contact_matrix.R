a205d2a2f996562788cc611e5ef540961a65adf8
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2023-02-16 15:22:33 +0000

    restart from basics

--- R/contact_matrix.R
+++ R/contact_matrix.R
@@ -1,201 +1,119 @@
 #' @importFrom stats setNames
+#' @importFrom withr with_collate
+#' @import data.table
 NULL
 
-#' Create a social contact matrix
+#' Contact matrix constructor
 #'
-#' `social_contact_matrix()` creates a social contract matrix from `i`, `j` and
-#' `value` vectors following the definition of given in \cite{Klepac et al}.
+# -------------------------------------------------------------------------
+#' `contact_matrix()` creates a social contract matrix from long form input.
 #'
-#' @param i `[character]`.
-#' Vector of grouping that represent the *ith* (row) in \eqn{m_{ij}} contacts.
+# -------------------------------------------------------------------------
+#' @param from `[list]`
 #'
-#' @param j `[character]`.
-#' Vector of grouping that represent the *jth* (column) in \eqn{m_{ij}} contacts.
+#' @param to `[list]`
 #'
-#' @param value `[numeric]`.
-#' Vector of values corresponding corresponding to inputs `i` and `j`.
+#' @param value `[numeric]`
 #'
-#' @param groups `[character]`.
-#' Possible groups that can appear in `i` and `j`. This will be used as the
-#' `dimnames()` of the resulting contact matrix. By default this is set to
-#' unique(c(i, j)).
+#' @param order `[character]`
 #'
-#' @param from `[character]`.
-#' Should the contact matrix be interpreted as from "columns" to rows or from
-#' "rows" to columns. Defaults to `from = "columns"`.
+#' @param fill `[numeric]`
 #'
-#' @param fill `[numeric]`.
-#' Numeric scalar used to fill empty values in the resulting contact matrix.
-#' Defaults to 0.
+# -------------------------------------------------------------------------
 #'
 #' @export
-social_contact_matrix <- function(
-    i = character(),
-    j = character(),
+contact_matrix <- function(
+    from = list(),
+    to = list(),
     value = numeric(),
-    groups = unique(c(i, j)),
-    from = c("columns", "rows"),
+    order = NULL,
     fill = 0
 ) {
-    # check rows are both characters or both factors
-    if (is.factor(i) && is.factor(j)) {
-        i <- as.character(i)
-        j <- as.character(j)
-    } else if (!(is.character(i) && is.character(j))) {
-        stop("`i` and `j` must both be character or factor vectors.")
-    }
 
-    # ensure from is valid
-    from <- match.arg(from)
+    # value checking
+    if(!is.numeric(value))
+        stop("`value` must be <numeric>.")
 
-    # check fill
-    if (!(is.numeric(fill) && length(fill) == 1L))
-        stop("`fill` must be a numeric scalar value.")
+    # from checks
+    if (!is.list(from))
+        stop("`from` must be a list.")
 
-    # check for compatible row and column lengths
-    ni <- length(i)
-    nj <- length(j)
-    if (ni != nj)
-        stop("`i` and `j` must be of equal length.")
+    length_from <- length(from)
+    if (!length(from))
+        stop("`from` cannot be empty.")
 
-    # check values are valid
-    if (!is.numeric(value) || length(value) != ni)
-        stop("`value` must be numeric and of the same length as `i` (and `j`).")
+    nrows_from <- unique(lengths(from))
+    if (length(nrows_from) > 1L)
+        stop("all variables in `from` must have the same length.")
 
-    # check groups or generate if NULL
-    possible_groups <- unique(c(i, j))
-    if (is.null(groups)) {
-        groups <- possible_groups
-    } else if (!(is.factor(groups) || is.character(groups))) {
-        stop("`groups` must be a factor or character vector.")
+    names_from <- names(from)
+    if (is.null(names(from)))
+        stop("all variables in `from` must be named.")
+    if (length(unique(names_from)) != length(names_from))
+        stop("all variables in `from` must be uniquely named.")
+    if (any(names_from == ""))
+        stop("all variables in `from` must be uniquely named.")
+
+    # to checks
+    if (!is.list(to))
+        stop("`to` must be a list.")
+    length_to <- length(to)
+    if (length_from != length_to)
+        stop("`from` and `to` must have equal length.")
+
+    names_to <- names(to)
+    if (!setequal(names_to, names_from))
+        stop("`from` and `to` variables must have identical names.")
+
+    nrows_to <- unique(lengths(to))
+    if (length(nrows_to) > 1L || nrows_to != nrows_from)
+        stop("all variables in `to` must have the same length as those in `from`.")
+
+    # alias names
+    nms <- names_from
+
+    # TODO - custom high level ordering
+    if (!is.null(order)) {
+        stop("TODO - custom ordering is not yet implemented")
     } else {
-        if (anyDuplicated(groups))
-            stop("`groups` must be unique.")
-        missing <- possible_groups[!possible_groups %in% groups]
-        if (length(missing)) {
-            stop(sprintf(
-                "%s is present in `i` / `j` but not in `groups`",
-                sQuote(missing[1L])
-            ))
-        }
+        # use C locale
+        nms <- withr::with_collate("C", sort(nms))
+        from <- from[nms]
+        to <- to[nms]
     }
 
-    size <- length(groups)
-    out <- matrix(data = fill, nrow = size, ncol = size, dimnames = list(groups, groups))
-    index <- cbind(i, j)
-    out[index] <- value
-    .new_contact_matrix(out, from = from, class = "social_contact_matrix")
-}
+    # combine to and from to get possible groupings
+    # note using data.table should ensures C Locale sorting
+    .id <- NULL
+    possible <- .mapply(c, dots = list(from, to), MoreArgs = NULL)
+    possible <- do.call(CJ, c(unname(possible), sorted = TRUE, unique = TRUE))
+    setnames(possible, nms)
+    possible[, .id := .I]
 
-#' Create a population contact matrix
-#'
-#' `population_contact_matrix()` creates a social contract matrix from `i`, `j`
-#' and `value` vectors following the definition of given in \cite{Klepac et al}.
-#'
-#' @param i `[character]`.
-#' Vector of grouping that represent the *ith* (row) in \eqn{C_{ij}} contacts.
-#'
-#' @param j `[character]`.
-#' Vector of grouping that represent the *jth* (column) in \eqn{C_{ij}} contacts.
-#'
-#' @param value `[numeric]`.
-#' Vector of values corresponding corresponding to inputs `i` and `j`.
-#'
-#' @param groups `[character]`.
-#' Possible groups that can appear in `i` and `j`. This will be used as the
-#' `dimnames()` of the resulting contact matrix. By default this is set to
-#' unique(c(i, j)).
-#'
-#' @param population `[numeric]`.
-#' Vector of length the same as `groups` with value corresponding to the
-#' population size of group with the *kth* value corresponding to the *kth*
-#' entry in `groups`. Used to validate the correctness of the resulting
-#' contact matrix, that is \eqn{C_{ij} * w_{j} == C_{ji} * w_{i}}, where
-#' \eqn{w_{k}} represents the population in group *k*.
-#'
-#' @param from `[character]`.
-#' Should the contact matrix be interpreted as from "columns" to rows or from
-#' "rows" to columns. Defaults to `from = "columns"`.
-#'
-#' @param fill `[numeric]`.
-#' Numeric scalar used to fill empty values in the resulting contact matrix.
-#' Defaults to 0.
-#'
-#' @export
-population_contact_matrix <- function(
-    i = character(),
-    j = character(),
-    value,
-    groups = unique(c(i, j)),
-    population,
-    from = c("columns", "rows"),
-    fill = 0
-) {
-    # check rows are both characters or both factors
-    if (is.factor(i) && is.factor(j)) {
-        i <- as.character(i)
-        j <- as.character(j)
-    } else if (!(is.character(i) && is.character(j))) {
-        stop("`i` and `j` must both be character or factor vectors.")
-    }
+    # index from
+    from <- as.data.table(from)
+    from_idx <- possible[from, on = nms]
 
-    # ensure from is valid
-    from <- match.arg(from)
+    # index to
+    to <- as.data.table(to)
+    to_idx <- possible[to, on = nms]
 
-    # check fill
-    if (!(is.numeric(fill) && length(fill) == 1L))
-        stop("`fill` must be a numeric scalar value.")
-
-    # check for compatible row and column lengths
-    ni <- length(i)
-    nj <- length(j)
-    if (ni != nj)
-        stop("`i` and `j` must be of equal length.")
-
-    # check values are valid
-    if (!is.numeric(value) || length(value) != ni)
-        stop("`value` must be numeric and of the same length as `i` (and `j`).")
-
-    # check groups or generate if NULL
-    possible_groups <- unique(c(i, j))
-    if (is.null(groups)) {
-        groups <- possible_groups
-    } else if (!(is.factor(groups) || is.character(groups))) {
-        stop("`groups` must be a factor or character vector.")
-    } else {
-        groups <- unique(as.character(groups))
-        missing <- possible_groups[!possible_groups %in% groups]
-        if (length(missing)) {
-            stop(sprintf(
-                "%s is present in `i` / `j` but not in `groups`",
-                sQuote(missing[1L])
-            ))
-        }
-    }
+    # pull out groups
+    setDF(possible)
+    possible$.id <- NULL
 
     # create matrix
-    size <- length(groups)
-    out <- matrix(data = fill, nrow = size, ncol = size, dimnames = list(groups, groups))
-    index <- cbind(i, j)
-    out[index] <- value
+    dimnames <- do.call(paste, c(.subset(possible, nms), sep = " / "))
+    dimnames <- sprintf("[%s]", dimnames)
+    dimnames <- list(dimnames, dimnames)
+    nrows <- nrow(possible)
+    out <- matrix(fill, nrow = nrows, ncol = nrows, dimnames = dimnames)
+    idx <- cbind(from_idx$.id, to_idx$.id)
+    out[idx] <- value
 
-    # check population
-    if (!is.numeric(population) || length(population) != size)
-        stop("`value` must be numeric and of the same length as `groups`.")
-    if (from == "columns") {
-        weighted_out <- out * population[col(out)] / population[row(out)]
-    } else {
-        weighted_out <- out * population[row(out)] / population[col(out)]
-    }
-    if (!isTRUE(all.equal(out, t(weighted_out))))
-        stop("`population` values incompatible with specified `i`, `j` and `values`.")
-
-    # add population information as attribute and return
-    out <- .new_contact_matrix(out, from = from, class = "population_contact_matrix")
-    attr(out, "population") <- setNames(population, groups)
-    out
+    # encode groups in attribute
+    structure(out, groups = possible, class = "contact_matrix")
 }
-
 
 #' @export
 print.contact_matrix <- function(x, ...) {
@@ -203,147 +121,20 @@ print.contact_matrix <- function(x, ...) {
     # copy so we can return invisibly at end
     original <- x
 
-    # format from and to
-    from <- attr(x, "from")
-    if (from == "columns") {
-        to <- "rows"
-    } else if (from == "rows") {
-        to <- "columns"
-    } else {
-        stop("Corrupt contract_matrix: invalid `from` attribute")
-    }
+    # strip groups attribute for printing purposes
+    attr(x, "groups") <- NULL
 
-    # get dimensions
+    # print meta information
     d <- dim(x)
-
-    # print meta
-    cat(sprintf("From: %s\n", from))
-    cat(sprintf("  To: %s\n", to))
-    cat(sprintf(" dim: %d x %d\n\n", d[1], d[2]))
+    cat(sprintf("dimensions: %d x %d\n", d[1], d[2]))
+    groups <- attr(original, "groups")
+    groups <- paste(names(groups), collapse = ", ")
+    cat(sprintf("groups:     %s\n\n", groups))
 
     # strip class and from attribute prior to printing
-    attr(x, "from") <- NULL
     print(unclass(x), ...)
 
     # return input invisibly
     invisible(original)
 }
 
-#' @export
-print.population_contact_matrix <- function(x, ...) {
-
-    # copy so we can return invisibly at the end
-    original <- x
-
-    # strip population attribute for printing purposes
-    attr(x, "population") <- NULL
-
-    # dispatch to contact_matrix printing
-    print.contact_matrix(x, ...)
-
-    # print population
-    cat("\nPopulation:\n")
-    print(attr(original, "population"))
-
-    # return input invisibly
-    invisible(original)
-}
-
-
-
-#' Coerce to a population contact matrix
-#'
-#' `as_population_matrix()` is a generic for coercing an input to a population
-#' contact matrix.
-#'
-#' @param x An \R object..
-#' Vector of grouping that represent the *ith* (row) in \eqn{m_{ij}} contacts.
-#'
-#' @param population `[numeric]`.
-#' Vector of length equal to the number of rows (columns) in `x`. The *kth*
-#' entry corresponds to the group represented by the *kth* row of `x`.
-#'
-#' @param ...
-#' Additional arguments to be passed to or from methods.
-#'
-#' @export
-as_population_matrix <- function(x, ...) UseMethod("as_population_matrix")
-
-#' @rdname as_population_matrix
-#' @export
-as_population_matrix.social_contact_matrix <- function(x, population, ...) {
-    from = attr(x, "from")
-    if (from == "columns") {
-        out <- (x + t(x) * population[row(x)] / population[col(x)]) / 2
-    } else {
-        # x has equal dimensions so row(x) == row(t(x)) and col(x) == col(t(x))
-        out <- t((t(x) + x * population[row(x)] / population[col(x)]) / 2)
-    }
-
-    # add population information as attribute and return
-    groups <- dimnames(out)[[1]]
-    out <- .new_contact_matrix(out, from = from, class = "population_contact_matrix")
-    attr(out, "population") <- setNames(population, groups)
-    out
-
-}
-
-#' @export
-as.matrix.contact_matrix <- function(x, ...) {
-    dim <- dim(x)
-    attributes(x) <- NULL
-    dim(x) <- dim
-    x
-}
-
-# WIP - this would be the external function for other developers building
-#       contact matrices. Has a few additional checks on input. I've not used
-#       for the the social and raw contact matrices construction but this could
-#       change. Not yet exported.
-#
-# Create a contact matrix
-new_contact_matrix <- function(m, from = c("columns", "rows"), class = NULL) {
-
-    # check matrix input
-    if (!is.matrix(m))
-        stop("`x` must be a matrix")
-
-    # check valid from
-    from <- match.arg(from)
-
-    # check valid class
-    if (!is.character(class) || length(class) != 1L)
-        stop("`class` must be a string (character vector of length 1).")
-
-    # must be square
-    d <- dim(m)
-    if (d[1L] != d[2L])
-        stop(sprintf("contact matrices must be square but `m` is %d x %d.", d[1], d[2]))
-
-    # if it has dimnames they must be identical
-    nms <- dimnames(m)
-    if (!is.null(nms[[1L]]) && !identical(nms[[1]], nms[[2L]]))
-        stop("the row and column names of `m` must be identical (or NULL).")
-
-    # return contact matrix
-    .new_contact_matrix(m)
-
-}
-
-
-
-# -------------------------------------------------------------------------
-# -------------------------------------------------------------------------
-# Internals ---------------------------------------------------------------
-# -------------------------------------------------------------------------
-# -------------------------------------------------------------------------
-.new_contact_matrix <- function(m, from, class) {
-    new_attributes <- list(
-        dim = dim(m),
-        dimnames = dimnames(m),
-        from = from,
-        class = c(class, "contact_matrix")
-    )
-    attributes(m) <- new_attributes
-    m
-}
--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-Proof of concept (poc) for a contact matrix class that could underpin
-various packages
+WIP on proof of concept (poc) for a contact matrix class that could
+underpin various packages
 (e.g. [socialmixr](https://cran.r-project.org/package=socialmixr)).
 
 ## Installation
@@ -16,138 +16,33 @@ You can install cm from [GitHub](https://github.com/) with:
 
 ``` r
 # install.packages("remotes")
-remotes::install_github("epi-helpers/cm")
+remotes::install_github("TimTaylor/cm")
 ```
 
-## Overview
-
-### A contact matrix class
-
-The underlying `contact_matrix` class is very simple with the only
-requirements on the underlying data being that it must be a numeric
-matrix of equal dimensions (i.e. an equal number of rows and columns).
-Encoded with this data is an additional `from` attribute which conveys
-to a user whether the matrix should be read as from “rows to columns” or
-from “columns to rows”.
-
-### Subclasses
-
-`cm` also provides two contact matrix subclasses based on those
-discussed in Klepec et al:
-
--   a `social_contact_matrix`; and
--   a `population_contact_matrix`.
-
-The motivation for explicit classes is to avoid confusion on behalf of
-package users/developers (e.g. are contacts represented as from “rows to
-columns” or “columns to rows”; is this matrix symmetric with respect to
-population weightings?).
-
-The subclasses deliberately simple. For social contact matrices these
-are just a subclass of a contact matrix. For population contact matrices
-they require an additional attribute that encapsulates the population
-weightings that give symmetry, that is , where represents the population
-in group *k*.
-
-### Example
-
-We use data from
-[socialmixr](https://cran.r-project.org/package=socialmixr) to
-understand how we could utilise explicit matrix classes. Currently
-construction is slightly long-winded as I still need to provide general
-coercion functions for ready formed matrices (remember this is just a
-poc).
+## Example
 
 ``` r
+library(tibble)
 library(cm)
-library(socialmixr)
-#> 
-#> Attaching package: 'socialmixr'
-#> The following object is masked from 'package:utils':
-#> 
-#>     cite
 
-# first obtain data for the social contact matrix and underlying population
-dat <- contact_matrix(
-    polymod,
-    countries = "United Kingdom",
-    age.limits = c(0, 1, 5, 15)
+dat <- tribble(
+        ~age,  ~gender,    ~age,  ~gender, ~value,
+     "young",   "male",   "old", "female",     1L,
+     "young", "female",   "old", "female",     2L,
+       "old", "female", "young", "female",     2L
 )
-#> Using POLYMOD social contact data. To cite this in a publication, use the 'cite' function
-#> Removing participants that have contacts without age information. To change this behaviour, set the 'missing.contact.age' option
-dat <- dat$matrix
-dat2 <- contact_matrix(
-    polymod,
-    countries = "United Kingdom",
-    age.limits = c(0, 1, 5, 15),
-    symmetric = TRUE
-)
-#> Using POLYMOD social contact data. To cite this in a publication, use the 'cite' function
-#> Removing participants that have contacts without age information. To change this behaviour, set the 'missing.contact.age' option
-#> Warning in pop_age(survey.pop, part.age.group.present, ...): Not all age groups represented in population data (5-year age band).
-#>   Linearly estimating age group sizes from the 5-year bands.
-popn <- dat2$demography$population
 
-# currently we require long format data
-(dat <- as.data.frame(as.table(dat)))
-#>    age.group contact.age.group       Freq
-#> 1      [0,1)             [0,1) 0.40000000
-#> 2      [1,5)             [0,1) 0.11250000
-#> 3     [5,15)             [0,1) 0.02450980
-#> 4        15+             [0,1) 0.03230337
-#> 5      [0,1)             [1,5) 0.80000000
-#> 6      [1,5)             [1,5) 1.93750000
-#> 7     [5,15)             [1,5) 0.50490196
-#> 8        15+             [1,5) 0.35814607
-#> 9      [0,1)            [5,15) 1.26666667
-#> 10     [1,5)            [5,15) 1.46250000
-#> 11    [5,15)            [5,15) 7.94607843
-#> 12       15+            [5,15) 1.29073034
-#> 13     [0,1)               15+ 5.93333333
-#> 14     [1,5)               15+ 5.45000000
-#> 15    [5,15)               15+ 6.21568627
-#> 16       15+               15+ 9.59410112
+from <- .subset(dat, 1:2)
+to <- .subset(dat, 3:4)
+value <- .subset2(dat, 5L)
 
-# create a social contact matrix
-(social_mat <- with(
-    dat,
-    social_contact_matrix(age.group, contact.age.group, Freq, from = "rows")
-))
-#> From: rows
-#>   To: columns
-#>  dim: 4 x 4
+contact_matrix(from, to, value)
+#> dimensions: 4 x 4
+#> groups:     age, gender
 #> 
-#>             [0,1)     [1,5)   [5,15)      15+
-#> [0,1)  0.40000000 0.8000000 1.266667 5.933333
-#> [1,5)  0.11250000 1.9375000 1.462500 5.450000
-#> [5,15) 0.02450980 0.5049020 7.946078 6.215686
-#> 15+    0.03230337 0.3581461 1.290730 9.594101
-
-# convert to a population contact matrix
-(population_mat <- as_population_matrix(social_mat, population = popn))
-#> From: rows
-#>   To: columns
-#>  dim: 4 x 4
-#> 
-#>             [0,1)     [1,5)   [5,15)      15+
-#> [0,1)  0.40000000 0.6250000 0.764365 4.122919
-#> [1,5)  0.15625000 1.9375000 1.406063 5.929829
-#> [5,15) 0.07148821 0.5260153 7.946078 7.428739
-#> 15+    0.05759306 0.3313352 1.109550 9.594101
-#> 
-#> Population:
-#>    [0,1)    [1,5)   [5,15)      15+ 
-#>   690734  2762936  7385454 49447627
-
-# compare this to the data already loaded from socialmixr
-(original <- dat2$matrix)
-#>       contact.age.group
-#>             [0,1)     [1,5)   [5,15)      15+
-#>   [1,] 0.40000000 0.6250000 0.764365 4.122919
-#>   [2,] 0.15625000 1.9375000 1.406063 5.929829
-#>   [3,] 0.07148821 0.5260153 7.946078 7.428739
-#>   [4,] 0.05759306 0.3313352 1.109550 9.594101
-dimnames(original) <- NULL
-all.equal(as.matrix(population_mat), original)
-#> [1] TRUE
+#>                  [old / female] [old / male] [young / female] [young / male]
+#> [old / female]                0            0                2              0
+#> [old / male]                  0            0                0              0
+#> [young / female]              2            0                0              0
+#> [young / male]                1            0                0              0
 ```
